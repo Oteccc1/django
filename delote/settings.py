32b69f675c182ec4609502c5932e978d359c989e
Oteccc1 <92978502+Oteccc1@users.noreply.github.com>
2022-06-08 01:18:47 +0300

    Update settings.py

--- delote/settings.py
+++ delote/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s97xxhh)fep097kzuct@g3&*s@8s(r5s1a*hthq$pngr2l2s+9'
+SECRET_KEY = #шыш
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
